413a88216cecb1352e11b7d3cdaa6c82a5e749d8
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-07-26 15:35:50 -0600

    FirstCommit

---
+++ Startups/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Startups.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
